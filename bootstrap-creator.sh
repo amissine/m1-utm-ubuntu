e32476a8669376e5671b558bce4a04068139b838
Alec Missine <alec_missine@yahoo.com>
2021-04-25 16:53:18 -0400

    Bug fix

--- bootstrap-creator.sh
+++ bootstrap-creator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-read; echo '$REPLY' >> ~/.ssh/authorized_keys
+cat >> ~/.ssh/authorized_keys
 # TODO ./users.awk run="echo $REPLY >> .ssh/authorized_keys" /etc/hosts
 cat ~/.ssh/id_ed25519.pub
