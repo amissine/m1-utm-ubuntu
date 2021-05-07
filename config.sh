73cf926b2d2886affbb2706d1bd040e713391819
Alec Missine <alec_missine@yahoo.com>
2021-05-07 14:46:14 +0000

    Install docker

--- config.sh
+++ config.sh
@@ -10,13 +10,26 @@ vim_config_update () { # {{{1
   echo '    ...done'; echo
 }
 
+install_docker () { # {{{1
+  # https://linuxize.com/post/how-to-install-and-use-docker-on-ubuntu-20-04/
+  sudo -E apt-get install apt-transport-https ca-certificates gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
+  sudo usermod -aG docker $USER
+}
+
 install_packages () { # {{{1
   echo "  - installing packages..."
 
   export DEBIAN_FRONTEND=noninteractive
+  sudo apt-get install ntp # to sync time before apt-get update
+  sudo sntp time.nist.gov  #
+  timedatectl              #
   sudo -E apt-get update && \
     sudo -E apt-get -y install build-essential # autoconf automake libtool
 
+  install_docker
   make
 
   echo '    ...done'; echo
